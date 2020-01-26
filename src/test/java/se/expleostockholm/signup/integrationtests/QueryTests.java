943413884d6ade9f42ad165e89f5c413a4b0210e
Jan Grape <jan.grape@crisp.se>
2020-01-26 20:04:38 +0100

    Added integration test case for GraphQL

---
+++ src/test/java/se/expleostockholm/signup/integrationtests/QueryTests.java
@@ -0,0 +1,37 @@
+package se.expleostockholm.signup.integrationtests;
+
+import com.graphql.spring.boot.test.GraphQLResponse;
+import com.graphql.spring.boot.test.GraphQLTestTemplate;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(initializers = {SignupDbTests.Initializer.class})
+class QueryTests extends SignupDbTests {
+
+  @Resource
+  private GraphQLTestTemplate graphQLTestTemplate;
+
+ @Test
+  public void allEventsTest() throws IOException {
+    GraphQLResponse response = graphQLTestTemplate.perform("queries/allEvents.graphql", null);
+
+    assertTrue(response.isOk());
+    assertEquals("1", response.get("$.data.allEvents[0].id"));
+    assertEquals("Marcus Event", response.get("$.data.allEvents[0].title"));
+    assertEquals("Party party.", response.get("$.data.allEvents[0].description"));
+
+    // todo: BUG: date is not read properly from db
+    //assertEquals("2020-01-24", response.get("$.data.allEvents[0].date_of_event"));
+  }
+}
