769558a7f5c08c82ae19021c6be7c6b56e249259
Kristian Grundström <Kristian.Grundstrom@yh.nackademin.se>
2020-02-11 13:21:45 +0100

    Changes to gradle and travis. Logging failing test.

--- src/main/java/se/expleostockholm/signup/domain/Invitation.java
+++ src/main/java/se/expleostockholm/signup/domain/Invitation.java
@@ -1,14 +1,12 @@
 package se.expleostockholm.signup.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Invitation {
     private Long id;
     private Person guest;
--- src/test/java/se/expleostockholm/signup/integrationtests/InvitationMapperTest.java
+++ src/test/java/se/expleostockholm/signup/integrationtests/InvitationMapperTest.java
@@ -1,6 +1,10 @@
 package se.expleostockholm.signup.integrationtests;
 
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -16,6 +20,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @Testcontainers
 @SpringBootTest
 @ContextConfiguration(initializers = {SignupDbTests.Initializer.class})
+@Slf4j
 class InvitationMapperTest extends SignupDbTests {
 
     @Resource
@@ -25,12 +30,24 @@ class InvitationMapperTest extends SignupDbTests {
     void verify_getInvitation() {
         Optional<Invitation> invitation = invitationMapper.getInvitationById(1L);
 
+        Logger logger = LoggerFactory.getLogger(InvitationMapperTest.class);
+
+        logger.error("---------------\n" + invitation.get().toString());
+
+        assertTrue(invitation.isPresent(), "No invitation found!");
+        assertEquals(6L, invitation.get().getEvent().getId(), "Event id did not match!");
+        assertEquals(40L, invitation.get().getGuest().getId(), "Guest id did not match!");
+        assertEquals(Attendance.NO_RESPONSE, invitation.get().getAttendance(), "Attendance did not match!");
+
+                /*
         assertAll(
                 () -> assertTrue(invitation.isPresent(), "No invitation found!"),
                 () -> assertEquals(6L, invitation.get().getEvent().getId(), "Event id did not match!"),
                 () -> assertEquals(40L, invitation.get().getGuest().getId(), "Guest id did not match!"),
                 () -> assertEquals(Attendance.NO_RESPONSE, invitation.get().getAttendance(), "Attendance did not match!")
         );
+
+                 */
     }
 
     @Test
