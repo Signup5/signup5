65f709b8ef1e626d9ab51b307c9b6c9abfc6595f
Kristian Grundström <Kristian.Grundstrom@yh.nackademin.se>
2020-02-19 14:41:58 +0100

    Added GraphQLError exception handling for not found resources.

--- src/main/java/se/expleostockholm/signup/exception/InvitationNotFoundException.java
+++ src/main/java/se/expleostockholm/signup/exception/InvitationNotFoundException.java
@@ -1,7 +1,23 @@
 package se.expleostockholm.signup.exception;
 
-public class InvitationNotFoundException extends RuntimeException {
+import graphql.ErrorClassification;
+import graphql.GraphQLError;
+import graphql.language.SourceLocation;
+
+import java.util.List;
+
+public class InvitationNotFoundException extends RuntimeException implements GraphQLError  {
     public InvitationNotFoundException(String message) {
         super(message);
+    }
+
+    @Override
+    public List<SourceLocation> getLocations() {
+        return null;
+    }
+
+    @Override
+    public ErrorClassification getErrorType() {
+        return null;
     }
 }
--- src/main/java/se/expleostockholm/signup/exception/PersonNotFoundException.java
+++ src/main/java/se/expleostockholm/signup/exception/PersonNotFoundException.java
@@ -1,6 +1,22 @@
 package se.expleostockholm.signup.exception;
 
-public class PersonNotFoundException extends RuntimeException {
+import graphql.ErrorClassification;
+import graphql.GraphQLError;
+import graphql.language.SourceLocation;
+
+import java.util.List;
+
+public class PersonNotFoundException extends RuntimeException implements GraphQLError {
     public PersonNotFoundException(String message) { super(message);
+    }
+
+    @Override
+    public List<SourceLocation> getLocations() {
+        return null;
+    }
+
+    @Override
+    public ErrorClassification getErrorType() {
+        return null;
     }
 }
