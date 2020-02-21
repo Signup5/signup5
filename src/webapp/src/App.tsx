9865cf538b7529660f86eb2bac04ecbe6e965920
Kristian Grundström <Kristian.Grundstrom@yh.nackademin.se>
2020-02-21 17:23:55 +0100

    First React component with GraphQL call.

--- src/webapp/src/App.tsx
+++ src/webapp/src/App.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React from 'react'
 import LoginForm from './Components/LoginForm'
-import './App.css';
-import ApolloClient from "apollo-boost";
-import gql from "graphql-tag";
+import './App.css'
+import { ApolloProvider } from "react-apollo"
+import { useQuery } from '@apollo/react-hooks'
+import ApolloClient from 'apollo-boost'
+import AllPersons from './Components/AllPersons'
+
+const client = new ApolloClient({
+  uri: "https://signup5-dev.herokuapp.com/graphql"
+});
 
 
 function App() {
+
   return (    
+    <ApolloProvider client={client}>
     <div className="App">
+
         <LoginForm />
+        <AllPersons />
+       
     </div>
+    </ApolloProvider>
   );
 }
 
--- src/webapp/src/index.tsx
+++ src/webapp/src/index.tsx
@@ -3,34 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { ApolloProvider } from "react-apollo";
-import ApolloClient from "apollo-boost";
-import gql from "graphql-tag";
 
-const client = new ApolloClient({
-  uri: "https://signup5-dev.herokuapp.com/graphql"
-});
-
-const testQuery = gql`
-  {
-    getAllPersons {
-      id
-      first_name
-      last_name
-      email
-    }
-  }
-`;
-
-client.query({
-    query: testQuery
-  })
-  .then(res => console.log(res));
+// client.query({
+//     query: testQuery
+//   })
+//   .then(res => console.log(res));
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+    <App />,
   document.getElementById("root")
 );
 
