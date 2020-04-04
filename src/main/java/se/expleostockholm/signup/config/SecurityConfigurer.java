7811e0933d74c53d0301ad75c7023d968d7cd18b
David Södergren <david.sodergren@pricer.com>
2020-04-04 11:30:18 +0200

    fixed cors issue

--- src/main/java/se/expleostockholm/signup/config/SecurityConfigurer.java
+++ src/main/java/se/expleostockholm/signup/config/SecurityConfigurer.java
@@ -27,6 +27,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+    http.cors();
 
     http.csrf().disable()
         .authorizeRequests()
