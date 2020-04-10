fc646595d364d873169c8642d950ba5a6076ef76
David Södergren <david.sodergren@pricer.com>
2020-04-10 16:28:55 +0200

    asd

--- src/main/java/se/expleostockholm/signup/config/SecurityConfigurer.java
+++ src/main/java/se/expleostockholm/signup/config/SecurityConfigurer.java
@@ -29,6 +29,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf().disable()
+        .cors().and()
         .authorizeRequests()
         .antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
         .antMatchers("/login", "/password/**", "/graphiql/**", "/vendor/**")
